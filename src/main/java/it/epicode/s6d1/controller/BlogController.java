d9b5ceb2ae94b71b6034c2e63c9c91734da64c49
MarcoZiino96 <141355693+MarcoZiino96@users.noreply.github.com>
2024-02-13 17:07:07 +0100

    consegna s6d1

--- src/main/java/it/epicode/s6d1/controller/BlogController.java
+++ src/main/java/it/epicode/s6d1/controller/BlogController.java
@@ -1,12 +1,50 @@
 package it.epicode.s6d1.controller;
 
 
+import it.epicode.s6d1.model.Autore;
+import it.epicode.s6d1.model.Blog;
 import it.epicode.s6d1.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 public class BlogController {
     @Autowired
     BlogService blogService;
+
+    @GetMapping("/blog")
+    public List<Blog> getAllblogs(){
+       return blogService.searchAllBlogs();
+    }
+
+    @GetMapping("/blog/{id}")
+    public ResponseEntity<Blog> getBlog(@PathVariable int id){
+        try{
+            Blog b = blogService.searchBlogForId(id);
+            return new ResponseEntity<>(b, HttpStatus.OK);
+        }
+        catch (NoSuchElementException e){
+            return  new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/blog")
+    public void saveBlog(@RequestBody Blog blog){
+        blogService.saveBlog(blog);
+    }
+
+    @PutMapping("/blog/{id}")
+    public Blog updateBlog(@PathVariable int id, Blog blog){
+        return blogService.updateBlog(id,blog);
+    }
+
+    @DeleteMapping("/blog/{id}")
+    public void deleteBlog(int id){
+        blogService.deleteBlog(id);
+    }
 }
